49470823ea4997376edcd54e6963a8d70cd9a68a
Matheus Zuchi Balbinot <104937774+MatheusZuchiBalbinot@users.noreply.github.com>
2025-07-21 22:09:05 -0300

    tweak: prettier format changes (#21)
    * chore: adding tailwind, changing Dockerfile, new packets and more
    
    * tweak: prettier format changes

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -2,10 +2,10 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('api')
 export class AppController {
-    @Get('/')
-    root() {
-        return { message: 'API está no ar' };
-    }
+	@Get('/')
+	root() {
+		return { message: 'API está no ar' };
+	}
 
 	@Get('hello')
 	getHello() {
