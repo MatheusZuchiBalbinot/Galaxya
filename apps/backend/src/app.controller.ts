7fbfca84c8dce34a38c95b18c4d89c4159024669
MatheusBalbinotZuchi <matheusbalbinotzuchi@gmail.com>
2025-07-21 22:04:03 -0300

    chore: adding tailwind, changing Dockerfile, new packets and more

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -2,6 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('api')
 export class AppController {
+    @Get('/')
+    root() {
+        return { message: 'API está no ar' };
+    }
+
 	@Get('hello')
 	getHello() {
 		return { message: 'Hello from NestJS!' };
