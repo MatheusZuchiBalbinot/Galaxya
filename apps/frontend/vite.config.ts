43186f28de2d067e18d683d64c5e4a8c5dcaeb0e
MatheusBalbinotZuchi <matheusbalbinotzuchi@gmail.com>
2025-09-06 21:32:31 -0300

    tweak: removing some code comments

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
 		host: '0.0.0.0',
 		port: 5173,
 		proxy: {
-			'/api': 'http://backend:3000',
+			// '/api': 'http://backend:3000',
+			'/api': 'http://localhost:3000', // backend runing locally
 		},
 	},
 });
